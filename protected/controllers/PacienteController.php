a3358c29b4274ac89a5d59a68b695c03feab4cae
Cristian <cristianedosoed@gmail.com>
2016-06-07 21:40:33 +0200

    Registro de paciente

--- protected/controllers/PacienteController.php
+++ protected/controllers/PacienteController.php
@@ -16,6 +16,18 @@ class PacienteController extends Controller
 		$this->renderPartial('_pacientListView',['pacienteList'=>$pacienteList]);
 	}
 
+	public function actionPacientCreate()
+	{
+		$modelPerson = new PersonaForm();
+		if (isset($_POST['PersonaForm'])) {
+			$modelPerson->attributes = $_POST['PersonaForm'];
+			//$modelPerson->scenario='paciente';
+			$modelPerson->savePaciente();
+
+		}
+		$this->render('pacientCreate', array('modelPerson' => $modelPerson));
+	}
+
 	// Uncomment the following methods and override them if needed
 	/*
 	public function filters()
