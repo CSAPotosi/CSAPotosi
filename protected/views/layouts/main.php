83955dc1490fbbb89b35d3f779d8e6e8fbd4c680
PACF <pedro.acf.3@gmail.com>
2016-12-11 23:31:11 -0400

    Mi envio

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -57,67 +57,29 @@
 
 		<!-- Note: The activity badge color changes when clicked and resets the number to 0
 			 Suggestion: You may want to set a flag when this happens to tick off all checked messages / notifications -->
-
-		<!-- AJAX-DROPDOWN : control this dropdown height, look and feel from the LESS variable file -->
-		<div class="ajax-dropdown">
-
-			<!-- the ID links are fetched via AJAX to the ajax container "ajax-notifications" -->
-			<div class="btn-group btn-group-justified" data-toggle="buttons">
-				<label class="btn btn-default">
-					<input type="radio" name="activity" id="ajax/notify/mail.html">
-					Msgs (14) </label>
-				<label class="btn btn-default">
-					<input type="radio" name="activity" id="ajax/notify/notifications.html">
-					notify (3) </label>
-				<label class="btn btn-default">
-					<input type="radio" name="activity" id="ajax/notify/tasks.html">
-					Tasks (4) </label>
-			</div>
-
-			<!-- notification content -->
-			<div class="ajax-notifications custom-scroll">
-
-				<div class="alert alert-transparent">
-					<h4>Haga click en un boton para ver los mensajes</h4>
-					Esta pagina en blanco ayuda a proteger su privacidad.
-				</div>
-
-				<i class="fa fa-lock fa-4x fa-border"></i>
-
-			</div>
-			<!-- end notification content -->
-
-			<!-- footer: refresh area -->
-			<span> Ultim actualizacion: 13/05/2016 12:12 AM
-			<button type="button" data-loading-text="<i class='fa fa-refresh fa-spin'></i> Cargando..." class="btn btn-xs btn-default pull-right">
-				<i class="fa fa-refresh"></i>
-			</button> </span>
-			<!-- end footer -->
-
-		</div>
-		<!-- END AJAX-DROPDOWN -->
 	</div>
 
 	<!-- #PROJECTS: projects dropdown -->
 	<div class="project-context hidden-xs">
 
-		<span class="label">Projects:</span>
+		<span class="label">Usuario:</span>
 		<span class="project-selector dropdown-toggle" data-toggle="dropdown">Recent projects <i class="fa fa-angle-down"></i></span>
 
 		<!-- Suggestion: populate this list with fetch and push technique -->
 		<ul class="dropdown-menu">
 			<li>
-				<a href="javascript:void(0);">Online e-merchant management system - attaching integration with the iOS</a>
+				<a href="javascript:void(0);">Mis datos</a>
 			</li>
 			<li>
-				<a href="javascript:void(0);">Notes on pipeline upgradee</a>
+				<a href="javascript:void(0);">Password</a>
 			</li>
 			<li>
-				<a href="javascript:void(0);">Assesment Report for merchant account</a>
+				<a href="javascript:void(0);">Reimprimir reportes</a>
 			</li>
 			<li class="divider"></li>
 			<li>
-				<a href="javascript:void(0);"><i class="fa fa-power-off"></i> Clear</a>
+				<a href="<?= CHtml::normalizeUrl(['site/logout']) ?>" data-action="userLogout"
+                   data-logout-msg="Puede mejorar su seguridad aun mas, cerrando el navegador despues de salir del sistema"><i class="fa fa-power-off"></i> Salir</a>
 			</li>
 		</ul>
 		<!-- end dropdown-menu-->
@@ -137,10 +99,8 @@
 
 		<!-- logout button -->
 		<div id="logout" class="btn-header transparent pull-right">
-			<span> <a href="<?php echo $this->createAbsoluteUrl('site/logout'); ?>" title="Salir"
-					  data-action="userLogout"
-					  data-logout-msg="Puede mejorar su seguridad aun mas, cerrando el navegador despues de salir del sistema"><i
-						class="fa fa-sign-out"></i></a> </span>
+			<span> <a href="<?php echo $this->createAbsoluteUrl('site/logout'); ?>" title="Salir" >
+                        <i class="fa fa-sign-out"></i></a> </span>
 		</div>
 		<!-- end logout button -->
 
@@ -154,7 +114,7 @@
 		<!-- input: search field -->
 		<form action="#" class="header-search pull-right">
 			<input id="search-fld" type="text" name="param" placeholder="Buscar">
-			<button type="submit">
+			<button type="button">
 				<i class="fa fa-search"></i>
 			</button>
 			<a href="javascript:void(0);" id="cancel-search-js" title="Cancelar busqueda"><i class="fa fa-times"></i></a>
@@ -181,7 +141,7 @@
 	<div class="login-info">
 		<span> <!-- User image size is adjusted inside CSS, it should stay as it -->
 			<a href="javascript:void(0);" id="show-shortcut" data-action="toggleShortcut">
-				<img src="img/avatars/sunny.png" alt="me" class="online" />
+				<img src="#" alt="me" class="online" />
 				<span>
 					<?php echo Yii::app()->user->getState("nombre"); ?>
 				</span>
@@ -297,10 +257,9 @@
 		</ul>
 	</nav>
 
-
-			<span class="minifyme" data-action="minifyMenu">
-				<i class="fa fa-arrow-circle-left hit"></i>
-			</span>
+    <span class="minifyme" data-action="minifyMenu">
+        <i class="fa fa-arrow-circle-left hit"></i>
+    </span>
 
 </aside>
 <!-- END NAVIGATION -->
@@ -311,14 +270,14 @@
 	<!-- RIBBON -->
 	<div id="ribbon">
 		<!-- breadcrumb -->
-		<ol class="breadcrumb">
-			<li><a href="#">AS</a></li><li>Miscellaneous</li><li>Blank Page</li>
-		</ol>
+        <?php  if(isset($this->breadcrumb)):?>
+        <ol class="breadcrumb">
+            <li><a href="#">AS</a></li><li>Miscellaneous</li><li>Blank Page</li>
+        </ol>
+        <?php endif;?>
 		<!-- end breadcrumb -->
 	</div>
 	<!-- END RIBBON -->
-
-
 
 	<!-- MAIN CONTENT -->
 	<div id="content">
@@ -358,82 +317,12 @@
 <!-- PAGE FOOTER -->
 <div class="page-footer">
 	<div class="row">
-		<div class="col-xs-12 col-sm-6">
+		<div class="col-xs-12 col-sm-12 text-align-center">
 			<span class="txt-color-white">CSA Potosi <span class="hidden-xs"> - Sistema</span> © 2016</span>
-		</div>
-
-		<div class="col-xs-6 col-sm-6 text-right hidden-xs">
-			<div class="txt-color-white inline-block">
-				<i class="txt-color-blueLight hidden-mobile">Ultima actividad <i class="fa fa-clock-o"></i> <strong>52 min. atras</strong> </i>
-				<div class="btn-group dropup">
-					<button class="btn btn-xs dropdown-toggle bg-color-blue txt-color-white" data-toggle="dropdown">
-						<i class="fa fa-link"></i> <span class="caret"></span>
-					</button>
-					<ul class="dropdown-menu pull-right text-left">
-						<li>
-							<div class="padding-5">
-								<p class="txt-color-darken font-sm no-margin">Download Progress</p>
-								<div class="progress progress-micro no-margin">
-									<div class="progress-bar progress-bar-success" style="width: 50%;"></div>
-								</div>
-							</div>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<div class="padding-5">
-								<p class="txt-color-darken font-sm no-margin"><a href="#">ASD</a></p>
-							</div>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<div class="padding-5">
-								<p class="txt-color-darken font-sm no-margin">Memory Load <span class="text-danger">*critical*</span></p>
-								<div class="progress progress-micro no-margin">
-									<div class="progress-bar progress-bar-danger" style="width: 70%;"></div>
-								</div>
-							</div>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<div class="padding-5">
-								<button class="btn btn-block btn-default">Actualizar</button>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
 		</div>
 	</div>
 </div>
 <!-- END PAGE FOOTER -->
-
-<!-- SHORTCUT AREA : With large tiles (activated via clicking user name tag)
-Note: These tiles are completely responsive,
-you can add as many as you like
--->
-<div id="shortcut">
-	<ul>
-		<li>
-			<a href="inbox.html" class="jarvismetro-tile big-cubes bg-color-blue"> <span class="iconbox"> <i class="fa fa-envelope fa-4x"></i> <span>Mail <span class="label pull-right bg-color-darken">14</span></span> </span> </a>
-		</li>
-		<li>
-			<a href="calendar.html" class="jarvismetro-tile big-cubes bg-color-orangeDark"> <span class="iconbox"> <i class="fa fa-calendar fa-4x"></i> <span>Calendar</span> </span> </a>
-		</li>
-		<li>
-			<a href="gmap-xml.html" class="jarvismetro-tile big-cubes bg-color-purple"> <span class="iconbox"> <i class="fa fa-map-marker fa-4x"></i> <span>Maps</span> </span> </a>
-		</li>
-		<li>
-			<a href="invoice.html" class="jarvismetro-tile big-cubes bg-color-blueDark"> <span class="iconbox"> <i class="fa fa-book fa-4x"></i> <span>Invoice <span class="label pull-right bg-color-darken">99</span></span> </span> </a>
-		</li>
-		<li>
-			<a href="gallery.html" class="jarvismetro-tile big-cubes bg-color-greenLight"> <span class="iconbox"> <i class="fa fa-picture-o fa-4x"></i> <span>Gallery </span> </span> </a>
-		</li>
-		<li>
-			<a href="profile.html" class="jarvismetro-tile big-cubes selected bg-color-pinkDark"> <span class="iconbox"> <i class="fa fa-user fa-4x"></i> <span>My Profile </span> </span> </a>
-		</li>
-	</ul>
-</div>
-<!-- END SHORTCUT AREA -->
 
 <!--================================================== -->
 
@@ -466,8 +355,6 @@ Yii::app()->clientScript->registerCoreScript('jquery.ui');
 
 <!-- FastClick: For mobile devices -->
 <script src="<?php echo Yii::app()->request->baseUrl;?>/resources/js/plugin/fastclick/fastclick.min.js"></script>
-
-
 
 
 <!--[if IE 8]>
