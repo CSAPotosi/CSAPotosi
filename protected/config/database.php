fb19eaed5ab2caf27dfe2c82d508d46d48d7a041
Pedro Alfonzo Condori Fernandez <pedro.acf.3@gmail.com>
2019-05-10 09:33:02 -0400

    db credentials

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,9 +4,9 @@
 return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	'connectionString' => 'pgsql:host=localhost;port=5432;dbname=csapotosi_db',
+	'connectionString' => 'pgsql:host=ec2-54-221-198-156.compute-1.amazonaws.com;port=5432;dbname=dalihpfosvhisi',
 	'emulatePrepare' => true,
-	'username' => 'postgres',
-	'password' => 'root',
+	'username' => 'djzerfymxmzvbk',
+	'password' => '48c7c0f9940b6e224a965d95c9a29923b224df305e2c849145232d6eec938d2a',
 	'charset' => 'utf8',
 );
